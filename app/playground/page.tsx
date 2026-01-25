c47d3c01854dfe9cea0c8a088043975ba2960f58
Shek Rasel <swe.rasel@gmail.com>
2026-01-26 00:41:32 +0600

    my playground

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -6,10 +6,20 @@ import Card from "@/components/cards/card";
 import Dialog from "@/components/dialogs/dialog";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import CircleLoader from "@/components/loaders/circle-loader";
+import AuthStepper from "@/components/steppers/auth-stepper";
+import { ProcessStepper } from "@/components/steppers/process-stepper";
 
 const Page = () => {
   const [open, setIsOpen] = useState<boolean>(false);
   const { control } = useForm();
+
+  const steps = [
+    { id: 1, title: "Property Details" },
+    { id: 2, title: "Visuals" },
+    { id: 3, title: "Services" },
+    { id: 4, title: "Review" },
+  ];
 
   const upazilas = [
     { label: "Dhanmondi", value: "dhanmondi" },
@@ -51,6 +61,19 @@ const Page = () => {
         options={upazilas}
         control={control}
       />
+
+      {/*  loaders test */}
+      <Button>
+        <CircleLoader />
+      </Button>
+
+      {/* steppers */}
+      <AuthStepper title="step 1 of 5" step={20} />
+
+      <div className="text-gray bg-primary h-40 w-full"></div>
+
+      <ProcessStepper steps={steps} currentStep={1} orientation="vertical" />
+      <ProcessStepper steps={steps} currentStep={2} orientation="horizontal" />
     </div>
   );
 };
