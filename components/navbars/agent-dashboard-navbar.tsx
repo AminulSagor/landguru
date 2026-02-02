8a17e32e5381b2c529bd5bdbf3052c4828848576
Shek Rasel <swe.rasel@gmail.com>
2026-02-02 16:26:32 +0600

    agent navbar

---
+++ components/navbars/agent-dashboard-navbar.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { Bell, Sandwich } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Path = {
+  name: string;
+  link: string;
+};
+
+const AgentDashbaordNavbar = () => {
+  const route = usePathname();
+  const paths: Path[] = [
+    {
+      name: "Home",
+      link: "/agent/dashboard",
+    },
+    {
+      name: "My Task",
+      link: "/agent/dashboard/task",
+    },
+    {
+      name: "Appointments",
+      link: "/agent/dashboard/appointment",
+    },
+  ];
+  return (
+    <nav className="flex py-4 lg:py-0 items-center justify-between bg-white responsive-padding shadow-md border-b border-gray/20 fixed w-full z-50">
+      <div className="flex gap-12">
+        <div className="flex items-center gap-2">
+          <span className="bg-primary rounded-lg text-white p-2 px-3">
+            <Sandwich size={18} />
+          </span>
+          <h1 className="font-bold text-base">LandGuru</h1>
+        </div>
+
+        {/* navlinks */}
+        <ul className="gap-4 lg:gap-8  items-center text-base text-gray hidden md:flex">
+          {paths.map((path) => {
+            const isActive = path.link === route;
+            return (
+              <li
+                className={`${isActive && "border-b-2 border-primary "} py-4`}
+                key={path.name}
+              >
+                <Link href={path.link}>{path.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      {/* setting area */}
+      <div className="flex items-center gap-3 text-base">
+        <Link href={"/agent/dashboard/notifications"}>
+          <Bell />
+        </Link>
+
+        {/* profile */}
+        <Link href={"/agent/dashboard/profile"}>
+          <div className="flex gap-2 items-center">
+            <h1>John Doe</h1>
+            <Image
+              src={"/images/avatars/avatar.png"}
+              height={28}
+              width={28}
+              className="rounded-full h-8 w-8 object-cover"
+              alt="Profile-image"
+            />
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default AgentDashbaordNavbar;
