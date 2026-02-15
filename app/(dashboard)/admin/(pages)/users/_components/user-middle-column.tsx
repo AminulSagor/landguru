b0f5f8cd7d1b4e0146a208cac7db250d124014c0
Shek Rasel <swe.rasel@gmail.com>
2026-02-15 23:12:40 +0600

    folder path change

--- app/(dashboard)/admin/(pages)/users/_components/user-middle-column.tsx
+++
@@ -1,6 +0,0 @@
-import UserPersonalDetailsCard from "@/app/(dashboard)/admin/(pages)/users/_components/user-personal-details-card";
-import { User } from "@/app/(dashboard)/admin/types/user-lists-types";
-
-export default function UserMiddleColumn({ user }: { user: User }) {
-  return <UserPersonalDetailsCard user={user} />;
-}
