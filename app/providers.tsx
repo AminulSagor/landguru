e5ed761cd8d9e5fe8eba8339f30410acb27ccbfd
Shek Rasel <swe.rasel@gmail.com>
2026-04-01 14:43:20 +0600

    query provider

---
+++ app/providers.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <Toaster position="top-center" />
+    </QueryClientProvider>
+  );
+}
